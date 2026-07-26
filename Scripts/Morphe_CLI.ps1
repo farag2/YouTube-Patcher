37080244b3ff362194f4fa404c906d34129b803c
Dmitry Nefedov <nefedovds@ya.ru>
2026-07-26 10:24:55 +0300

    Update Morphe_CLI.ps1

--- Scripts/Morphe_CLI.ps1
+++ Scripts/Morphe_CLI.ps1
@@ -1,20 +1,20 @@
-# https://github.com/MorpheApp/morphe-cli
+# https://github.com/MorpheApp/morphe-desktop
 
 $Parameters = @{
-	Uri             = "https://api.github.com/repos/MorpheApp/morphe-cli/releases/latest"
+	Uri             = "https://api.github.com/repos/MorpheApp/morphe-desktop/releases/latest"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
-$CLIMorphevTag = (Invoke-RestMethod @Parameters).tag_name
-$CLIMorpheTag = $CLIMorphevTag.replace("v", "")
+$MorpheDesktopvTag = (Invoke-RestMethod @Parameters).tag_name
+$MorpheDesktopTag = $MorpheDesktopvTag.replace("v", "")
 
 $Parameters = @{
-	Uri             = "https://github.com/MorpheApp/morphe-cli/releases/download/$CLIMorphevTag/morphe-cli-$CLIMorpheTag-all.jar"
-	Outfile         = "Morphe_Builder\morphe-cli.jar"
+	Uri             = "https://github.com/MorpheApp/morphe-desktop/releases/download/$MorpheDesktopvTag/morphe-desktop-$MorpheDesktopTag-all.jar"
+	Outfile         = "Morphe_Builder\morphe-desktop.jar"
 	Headers         = $Headers
 	UseBasicParsing = $true
 	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
-echo "CLIMorpheTag=$CLIMorpheTag" >> $env:GITHUB_ENV
+echo "MorpheDesktopTag=$MorpheDesktopTag" >> $env:GITHUB_ENV
